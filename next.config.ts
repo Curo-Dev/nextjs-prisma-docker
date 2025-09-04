2ccbb502353a6df5379f7672ab05082d3a00bcc8
curo <mail.curo.dev@gmail.com>
2025-09-05 01:43:00 +0900

    feat: tnwjd

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
